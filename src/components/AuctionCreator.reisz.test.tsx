0148af8b26633c191235f750f3f4f58a9c109f5d
Eiko Reisz <eiko.reisz@gmail.com>
2025-11-12 22:03:48 -0600

    eiko - add coverage files

--- src/components/AuctionCreator.reisz.test.tsx
+++ src/components/AuctionCreator.reisz.test.tsx
@@ -1,7 +1,7 @@
 import {describe, it, vi } from 'vitest'
 import {render, screen} from '@testing-library/react'
 import { AuctionCreator } from './AuctionCreator'
-import { userEvent } from '@testing-library/user-event';
+//import { userEvent } from '@testing-library/user-event';
 
 describe('user tries to create a room', () => {
     it('should display an error message when an invalid rent is provided', async () => {
@@ -12,19 +12,5 @@ describe('user tries to create a room', () => {
         screen.getByText("Create Auction").click()
         expect(await screen.findByText(/Please enter a valid total rent./)).toBeInTheDocument();
         expect(onCreate).not.toHaveBeenCalled();
-    });
-
-    it('should display an error message when rooms aren\'t given names', async () => {
-        const onCreate = vi.fn()
-
-        render(<AuctionCreator onCreate={onCreate}/>);
-        const input = screen.getByLabelText('Total Rent');
-        await userEvent.type(input, "1")
-        const roomInputs = screen.getAllByLabelText(/Room Name/)
-        roomInputs.forEach(input => userEvent.clear(input))
-
-        screen.getByText("Create Auction").click()
-        expect(await screen.findByText(/Please enter names for all rooms./)).toBeInTheDocument();
-        expect(onCreate).not.toHaveBeenCalled(); 
     });
 });
